18e0a3ffb95ad1bdb90fb3e25d541c3684a5b2f5
Koti <kgodagala@mdsol.com>
2019-04-26 13:54:16 -0400

    integrated the step count

--- app/src/main/java/com/medidata/mehealth/MainActivity.kt
+++ app/src/main/java/com/medidata/mehealth/MainActivity.kt
@@ -1,12 +1,41 @@
 package com.medidata.mehealth
 
+import android.content.Context
+import android.hardware.Sensor
+import android.hardware.SensorEvent
+import android.hardware.SensorEventListener
+import android.hardware.SensorManager
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import kotlinx.android.synthetic.main.activity_main.*
 
-class MainActivity : AppCompatActivity() {
+class MainActivity : AppCompatActivity(),SensorEventListener  {
+
+    var sensorManager : SensorManager? = null
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        sensorManager = getSystemService(Context.SENSOR_SERVICE) as SensorManager
+    }
+
+    override fun onResume() {
+        super.onResume()
+        var stepsSensor = sensorManager?.getDefaultSensor(Sensor.TYPE_STEP_COUNTER)
+        sensorManager?.registerListener(this, stepsSensor, SensorManager.SENSOR_DELAY_UI)
+    }
+
+    override fun onAccuracyChanged(p0: Sensor?, p1: Int) {}
+
+    override fun onSensorChanged(event: SensorEvent?) {
+        event?.let {
+            stepCount = it.values[0]
+            step.text = stepCount.toString()
+        }
+
+    }
+
+    companion object {
+        var stepCount: Float? = 0f
     }
 }
