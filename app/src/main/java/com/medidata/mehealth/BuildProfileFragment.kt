7c493d71e1bb757a7b51a22f7aaef03e3f33058d
Koti <kgodagala@mdsol.com>
2019-04-26 16:45:00 -0400

    random

--- app/src/main/java/com/medidata/mehealth/BuildProfileFragment.kt
+++ app/src/main/java/com/medidata/mehealth/BuildProfileFragment.kt
@@ -13,4 +13,9 @@ class BuildProfileFragment : Fragment() {
     override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
         return inflater.inflate(R.layout.fragment_build_profile, container, false)
     }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+    }
 }
