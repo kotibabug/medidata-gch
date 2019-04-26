071d999d522b1c996051cd81f259aaadf4cdcf22
Koti <kgodagala@mdsol.com>
2019-04-26 14:16:36 -0400

    intro screen

---
+++ app/src/main/java/com/medidata/mehealth/IntroActivity.kt
@@ -0,0 +1,11 @@
+package com.medidata.mehealth
+
+import android.os.Bundle
+import android.support.v7.app.AppCompatActivity
+
+class IntroActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+    }
+}
