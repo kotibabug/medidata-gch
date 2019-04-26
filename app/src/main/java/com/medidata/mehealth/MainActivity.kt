ceed74e17994b850d4ff1120f3e1e8b3f3c6b684
vir9474 <vintwala@mdsol.com>
2019-04-26 12:50:57 -0400

    added keystore and gsign in handler

--- app/src/main/java/com/medidata/mehealth/MainActivity.kt
+++ app/src/main/java/com/medidata/mehealth/MainActivity.kt
@@ -1,12 +1,18 @@
 package com.medidata.mehealth
 
+import android.content.Intent
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        btn_login.setOnClickListener {
+            startActivity(Intent(this, LoginActivity::class.java))
+        }
     }
 }
