1287a8dd7f11ab337b1a81583f9e536d8364b55f
vir9474 <vintwala@mdsol.com>
2019-04-26 16:31:24 -0400

    added spinner popup

--- app/src/main/java/com/medidata/mehealth/BuildProfileActivity.kt
+++ app/src/main/java/com/medidata/mehealth/BuildProfileActivity.kt
@@ -2,11 +2,26 @@ package com.medidata.mehealth
 
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Toast
+import kotlinx.android.synthetic.main.activity_build_profile.*
 
 class BuildProfileActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_build_profile)
+
+        edit_gender.setOnClickListener {
+            MDPopupDialog(this).apply {
+                startBtnHandler = {
+                    Toast.makeText(this@BuildProfileActivity, "male", Toast.LENGTH_SHORT).show()
+                }
+                endBtnHandler = {
+                    Toast.makeText(this@BuildProfileActivity, "female", Toast.LENGTH_SHORT).show()
+                }
+                create()
+                show()
+            }
+        }
     }
 }
