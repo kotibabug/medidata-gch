6d3c9d76f96a850eb8b94563a12b38ddd403cda9
vir9474 <vintwala@mdsol.com>
2019-04-26 17:00:03 -0400

    profile actvit

--- app/src/main/java/com/medidata/mehealth/BuildProfileFragment.kt
+++ app/src/main/java/com/medidata/mehealth/BuildProfileFragment.kt
@@ -1,11 +1,12 @@
 package com.medidata.mehealth
 
-import android.net.Uri
 import android.os.Bundle
 import android.support.v4.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Toast
+import kotlinx.android.synthetic.main.fragment_build_profile.*
 
 class BuildProfileFragment : Fragment() {
 
@@ -17,5 +18,17 @@ class BuildProfileFragment : Fragment() {
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
 
+        edit_gender.setOnClickListener {
+            MDPopupDialog(activity).apply {
+                startBtnHandler = {
+                    Toast.makeText(context, "male", Toast.LENGTH_SHORT).show()
+                }
+                endBtnHandler = {
+                    Toast.makeText(context, "female", Toast.LENGTH_SHORT).show()
+                }
+                create()
+                show()
+            }
+        }
     }
 }
