74e89d59c49fdce038858c4de43d7f781b151bab
vir9474 <vintwala@mdsol.com>
2019-04-26 11:53:11 -0400

    add google sign in dependency

--- app/src/main/java/com/medidata/mehealth/LoginActivity.kt
+++ app/src/main/java/com/medidata/mehealth/LoginActivity.kt
@@ -2,11 +2,18 @@ package com.medidata.mehealth
 
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import com.google.android.gms.auth.api.signin.GoogleSignInOptions
 
 class LoginActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_login)
+
+        // Configure sign-in to request the user's ID, email address, and basic
+        // profile. ID and basic profile are included in DEFAULT_SIGN_IN.
+        val gso = GoogleSignInOptions.Builder(GoogleSignInOptions.DEFAULT_SIGN_IN)
+            .requestEmail()
+            .build()
     }
 }
